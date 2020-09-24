0ac61bcc0053634ff0c634fa313ccd7737a03bb9
aylencandela <aylencandela@hotmail.com>
2020-09-24 12:38:28 -0300

    Tablas attend y loyal

--- Modulo 2/Tarea 4/js/main.js
+++ Modulo 2/Tarea 4/js/main.js
@@ -5,7 +5,14 @@ const app = new Vue ({
         members: [],
         checkedParties: ["D", "R", "ID"],
         states: [],
-        selectedState: 'all'
+        selectedState: 'all',
+        democrats: [],
+        republicans: [],
+        independents: [],
+        leastEngaged: [],
+        mostEngaged: [],
+        leastLoyal: [],
+        mostLoyal: []
     },
 
     methods: {
@@ -15,6 +22,35 @@ const app = new Vue ({
             }else if(document.getElementById('house')){
                 return 'https://api.propublica.org/congress/v1/113/house/members.json'
             }
+        },
+        getStates: function(){
+            for(let i=0; i<this.members.length;i++){
+                if(!this.states.includes(this.members[i].state)){
+                    this.states.push(this.members[i].state);
+                }
+            }
+        },
+        average: function(party, key){
+            let avg = 0
+            let sum = 0
+            for (let i = 0; i < party.length; i++) {
+                sum += party[i][key]
+            }
+            avg = Math.round(sum / party.length || "0")
+            return avg + "%"
+        },
+        findLoyalOrEngaged: function(key, least, most){
+            let tenPercent = Math.round(this.members.length/10)
+            let sorted = [...this.members]
+            sorted.sort((m1,m2) => {return m1[key] - m2[key]})
+        
+            let votesAtLowestTenPct = sorted[tenPercent][key]
+            let votesAtHighestTenPct = sorted[sorted.length - tenPercent][key]
+            this[most] = sorted.filter(m => m[key] <= votesAtLowestTenPct)
+            this[least] = sorted.filter(m => m[key] >= votesAtHighestTenPct).reverse()
+        },
+        numberOfVotes: function(member,key){
+            return Math.round(member.total_votes * member[key] / 100)
         }
     },
 
@@ -29,11 +65,12 @@ const app = new Vue ({
         })
         .then(data => {
             this.members = data.results[0].members
-            for(let i=0; i<this.members.length;i++){
-                if(!this.states.includes(this.members[i].state)){
-                    this.states.push(this.members[i].state);
-                }
-            }
+            this.getStates()
+            this.democrats = this.members.filter(m=>m.party == "D")
+            this.republicans = this.members.filter(m=>m.party == "R")
+            this.independents = this.members.filter(m=>m.party == "ID")
+            this.findLoyalOrEngaged("missed_votes_pct", "leastEngaged", "mostEngaged")
+            this.findLoyalOrEngaged("votes_with_party_pct", "mostLoyal", "leastLoyal")
         })
         .catch(function(error){
             alert(error)
